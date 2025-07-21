32c0cab0a13a2227f8676ce93d0db1ebc89640cc
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-07-21 17:15:14 -0600

    [FIRST COMMIT] initial project upload

---
+++ src/test/java/mx/edu/utez/rbbackendcomite/RbBackendComiteApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.rbbackendcomite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbBackendComiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
