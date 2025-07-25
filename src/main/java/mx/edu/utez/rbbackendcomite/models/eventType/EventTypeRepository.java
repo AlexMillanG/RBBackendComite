8a9e310de09ca2736ab812611ff73ff151cfec67
JGerardoToledoR <jgerardo.toledor@gmail.com>
2025-07-25 14:20:11 -0600

    [ADD] Event type Repository

--- src/main/java/mx/edu/utez/rbbackendcomite/models/eventType/EventTypeRepository.java
+++ src/main/java/mx/edu/utez/rbbackendcomite/models/eventType/EventTypeRepository.java
@@ -1,4 +1,9 @@
 package mx.edu.utez.rbbackendcomite.models.eventType;
 
-public class EventTypeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventTypeRepository extends JpaRepository<EventTypeEntity,Long> {
+    boolean existsByName(String b);
 }
