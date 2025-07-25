3a9d7bde30c90b395283c3f0808a80388f78ffe3
JGerardoToledoR <jgerardo.toledor@gmail.com>
2025-07-25 14:03:40 -0600

    [ADD]Archivos Event Type

---
+++ src/main/java/mx/edu/utez/rbbackendcomite/models/eventType/EventTypeEntity.java
@@ -0,0 +1,4 @@
+package mx.edu.utez.rbbackendcomite.models.eventType;
+
+public class EventTypeEntity {
+}
---
+++ src/main/java/mx/edu/utez/rbbackendcomite/services/eventType/EventTypeServices.java
@@ -0,0 +1,4 @@
+package mx.edu.utez.rbbackendcomite.services.eventType;
+
+public class EventTypeServices {
+}
