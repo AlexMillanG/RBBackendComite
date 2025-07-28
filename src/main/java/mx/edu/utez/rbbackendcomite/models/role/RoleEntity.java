ec1dc3630d8e7728d3fcbb0cc9715d35ed401db9
EmmaSA1 <132949371+EmmaSA1@users.noreply.github.com>
2025-07-28 16:05:54 -0600

    [UPDATE] se modifica la clase roleentity

--- src/main/java/mx/edu/utez/rbbackendcomite/models/role/RoleEntity.java
+++ src/main/java/mx/edu/utez/rbbackendcomite/models/role/RoleEntity.java
@@ -17,6 +17,30 @@ public class RoleEntity {
 
     private String name; // "ADMIN", "GROUP_ADMIN", "MEMBER"
 
+    public RoleEntity() {
+    }
+
+    public RoleEntity(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     /*@OneToMany(mappedBy = "role")
     private List<UserEntity> users = new ArrayList<>();*/
 
